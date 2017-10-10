f43201dc706572edb6b2dd5c526d637feeb6f313
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2017-10-10 13:45:47 +0200

    Build!

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 mkdir build/files -p
 cd build/files 
 wget -O- get.pharo.org/60+vmT | bash
-./pharo-ui Pharo eval --save "  Metacello new baseline: 'SmartInspect'; repository: 'github:///RMODINRIA-Blockchain/SmartInspect/src'; load. "
+./pharo-ui Pharo eval --save "  Metacello new baseline: 'SmartShackle'; repository: 'github://RMODINRIA-Blockchain/SmartShackle/src'; load. "
 cd ..
 cp ../*.sh .
 cp ../*.st .
